86cece0319c4cdbb3b9237eacc9fb9a211fd125d
ake456 <ansb70610@gmail.com>
2025-10-25 22:02:18 +0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,21 @@
+let display = document.getElementById('display');
+
+function addToDisplay(value) {
+display.value += value;
+}
+
+function clearDisplay() {
+display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    }
+    catch {
+        display.value = 'Error';
+    }   
+}
+
+
+
